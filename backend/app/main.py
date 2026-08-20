b20e8ec9bc9a2e9969d2378e2f5fa4ffda38d41e
Chris <rolletchris1@gmail.com>
2026-08-20 13:57:29 -0700

    remove imported function which has been implemented in follow up PR

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,10 +9,7 @@ from starlette.middleware.sessions import SessionMiddleware
 from app.config import get_settings
 from app.routers.auth_router import router as auth_router
 from app.routers.label_router import router as labels_router
-from app.routers.profile_router import (
-    public_router as public_profiles_router,
-    router as profile_router,
-)
+from app.routers.profile_router import router as profile_router
 from app.routers.project_router import router as projects_router
 from app.routers.project_type_router import router as project_types_router
 
